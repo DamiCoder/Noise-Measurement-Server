57b5161b9477a3c00efcc423b3ebdfd349b5ec88
damiancwiek <cwiekdam@gmail.com>
2020-07-05 14:59:18 +0200

    Fixed bug

--- src/main/java/pl/dcwiek/noisemeasurementserver/database/repository/StandardEntityRepository.java
+++ src/main/java/pl/dcwiek/noisemeasurementserver/database/repository/StandardEntityRepository.java
@@ -4,12 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.dcwiek.noisemeasurementserver.database.model.StandardEntity;
 import pl.dcwiek.noisemeasurementserver.database.model.TypeEntity;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface StandardEntityRepository extends JpaRepository<StandardEntity, Integer> {
-
-    List<StandardEntity> getAll();
 
     Optional<StandardEntity> findByTitleAndType(String title, TypeEntity type);
 
